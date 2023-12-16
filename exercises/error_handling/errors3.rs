981306e52f9da3e962136053df871b3706e41e4d
Eric Cline <escline@gmail.com>
2023-12-16 17:42:34 -0600

    Complete through errors

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -7,15 +7,17 @@
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 fn main() {
     let mut tokens = 100;
-    let pretend_user_input = "8";
+    let pretend_user_input = "a";
 
-    let cost = total_cost(pretend_user_input)?;
+    // let cost = total_cost(pretend_user_input)?;
+    let cost = match total_cost(pretend_user_input) {
+        Ok(c) => c,
+        Err(_) => return println!("invalid user input"),
+    };
 
     if cost > tokens {
         println!("You can't afford that many!");
